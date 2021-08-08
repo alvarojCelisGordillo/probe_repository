39f59cf94ab4996184a076f4a0e89a1adba86bd0
Alvaro <celistony@yahoo.com>
2021-08-07 19:38:07 -0500

    prueba no_1

---
+++ JS/app.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        name: "",
+        phone: "",
+        age: ""
+    },
+
+    methods: {
+
+        message() {
+            alert("Hola Como estas? que diablos haces puto!???")
+        }
+    }
+
+
+
+});
